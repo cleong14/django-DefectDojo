96f67970df5cf798c7274b2547c84e70a13746e6
Chaz Leong <13462818+cleong14@users.noreply.github.com>
2022-09-16 19:16:56 -1000

    fixed static_dirs env var (i think)

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -393,6 +393,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(os.path.dirname(DOJO_ROOT), 'components', 'node_modules'),
+    os.path.join(os.path.dirname(DOJO_ROOT), 'static', ''),
 )
 
 # List of finder classes that know how to find static files in
